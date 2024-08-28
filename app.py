8aa632d777cf44f19f0322fd7df4ef22324df168
Casey Kriutzfield <casey.kriutzfield@live.com>
2024-08-28 15:56:12 +0000

    Initial

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+import random
+
+#note, even though requests is part of the app, it is unused at the moment.
+# mostly bacause it's old and doesn't work.
+
+# print a random city from the cities.json file
+with open('cities.json') as f:
+    cities = json.load(f)
+
+# random number between 0 and the length of the list of cities
+rndCity = random.randint(0, len(cities) - 1)
+print(cities[rndCity]['city'])
